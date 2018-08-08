79276d025fbade4cf8c4880389f27d751d952d73
Fernanda Caldeira Takahashi <FeTaka@users.noreply.github.com>
2018-08-08 20:57:46 -0300

    Monte Carlo and parallel

--- monte_carlo_llr_nh.m
+++ monte_carlo_llr_nh.m
@@ -1,14 +1,13 @@
 %% GENERATE DISTRIBUTION OF NULL HYPOTHESIS
-function llr = monte_carlo_llr_nh(obj, X, D, gg, N)
+function llr = monte_carlo_llr_nh(X, D, N)
 nc = sum(D==1);
-Dd = zeros(size(D));
 llr = zeros(N,1);
-for i =1:N
-    Dd(:) = 0;
+parfor i =1:N
+    Dd = zeros(size(D));
     diseased = randperm(length(D), nc);
     Dd(diseased) = 1;
-    [obj, Gg] = gg_probability_func (X, Dd, 10);
-    [C, T, Allr] = scanning_gg_clusters (obj, X, Dd, gg);
+    [obj, gg] = gg_probability_func (X, Dd, 10);
+    [~, ~, Allr] = scanning_gg_clusters (obj, X, Dd, gg);
     llr(i) = max(Allr);
 end
 
