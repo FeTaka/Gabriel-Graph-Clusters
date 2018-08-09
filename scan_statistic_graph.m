ebae2ad445a99061a331693441deb3427fa94fec
Carla Takahashi <CarlaCTaka@users.noreply.github.com>
2018-08-09 20:44:47 -0300

    Corrections on Scan Statistics

--- scan_statistic_graph.m
+++ scan_statistic_graph.m
@@ -18,7 +18,7 @@ Sz = sum(connected_graph);%population in zone
 Cz = sum(D(connected_graph)==1); %cases in zone
 
 Mz = Sz*(C/length(D));
-if Mz < Cz
+if Mz <= Cz
     Iz = Cz/Mz;
     Oz = (C-Cz)/(C - Mz);
     scan_statistic_val = Cz*log(Iz) + (C - Cz)*log(Oz);
