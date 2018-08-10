d86807f2e2f219683c247128145fb85eabfc0a0a
Fernanda Caldeira Takahashi <FeTaka@users.noreply.github.com>
2018-08-09 22:34:22 -0300

    Revert "Merge branch 'master' of https://github.com/FeTaka/Gabriel-Graph-Clusters"
    This reverts commit 8f31a92754fab553951da50ac376bf453e8c6794, reversing
    changes made to e704d1c3fe7cd23a57a7e0ed19dff21390d0d550.

--- scan_statistic_graph.m
+++ scan_statistic_graph.m
@@ -12,16 +12,18 @@
 %%
 function scan_statistic_val = scan_statistic_graph (connected_graph, D)
 
-%All region
 C = sum(D==1);%total cases
-N=length(D);
-%Interest Zone
-Nz = sum(connected_graph);%population in zone
+
+Sz = sum(connected_graph);%population in zone
 Cz = sum(D(connected_graph)==1); %cases in zone
 
-scan_statistic_val = 0;
-if Cz/Nz > (C-Cz)/(N-Nz)
-    scan_statistic_val = Cz*log(Cz/Nz) + Cz*log((N-Nz)/(C-Cz))+ C*log((C-Cz)/(N-Nz)) + C*log(N/C);
+Mz = Sz*(C/length(D));
+if Mz < Cz
+    Iz = Cz/Mz;
+    Oz = (C-Cz)/(C - Mz);
+    scan_statistic_val = Cz*log(Iz) + (C - Cz)*log(Oz);
+else
+    scan_statistic_val = 0;
 end
 
 %scan_statistic_val = log( (Iz)^Cz * (Oz)^(C-Cz));
