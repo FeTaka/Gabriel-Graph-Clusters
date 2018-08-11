66345b79c0b3172c2ba406e29cf2818e8b6da393
Carla Takahashi <CarlaCTaka@users.noreply.github.com>
2018-08-11 16:28:23 -0300

    Zmatrix

--- Zmatrix_generator.m
+++ Zmatrix_generator.m
@@ -1,11 +1,14 @@
 %% This program is free software;
-%% 
-%% 
+%% File for Generating the Z Matrix
+%% Generates a matrix with the values of the PDF function for all domain
+%%
 %% -*- texinfo -*-
 %%
 
+%% Load Variables
 load('PDF_function.mat')
 
+%% Generate Z matrix
 zfun = @(x,y)pdf(obj,mapstd('apply',[x y]',ps)');
 [p,q] = meshgrid(-700:1:0, 0:1:700);
 Z = zeros(71,71);
@@ -16,5 +19,6 @@ for i=0:step:700
     end
 end
 
+%% Save Variables
 save('ZMatrix.mat', 'Z')
 
